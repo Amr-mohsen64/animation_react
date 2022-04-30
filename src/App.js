99ac46a09839008d7932565a875ec50c248cb622
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-04-30 14:01:17 +0200

    Added transition component on simple div

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Transition } from 'react-transition-group';
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -8,7 +8,8 @@ import List from "./components/List/List";
 class App extends Component {
 
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModalHandler = () => {
@@ -23,9 +24,29 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModalHandler} />
-        <Backdrop show={this.state.modalIsOpen} />
+        <button className="Button"
+          onClick={() => this.setState(prevState => ({ showBlock: !prevState.showBlock }))}>toggle div</button>
+        <br />
+
+        <Transition
+          in={this.state.showBlock} timeout={1000}
+          mountOnEnter
+          unmountOnExit>
+          {state => (<div
+            style={{
+              backgroundColor: 'red',
+              width: 100,
+              height: 100,
+              margin: 'auto',
+              transition: 'opacity 1s ease-out',
+              opacity: state === 'exiting' ? 0 : 1
+            }}></div>)}
+        </Transition>
         
+        {/* this behavior has limitations as it animatie in IN but not in OUT ,react dosent wait for animaton */}
+        {this.state.modalIsOpen && <Modal show={this.state.modalIsOpen} closed={this.closeModalHandler} />}
+        {this.state.modalIsOpen && <Backdrop show={this.state.modalIsOpen} />}
+
         <button
           className="Button"
           onClick={this.showModalHandler}>Open Modal</button>
